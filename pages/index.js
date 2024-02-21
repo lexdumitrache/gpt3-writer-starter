a913f4cff5e46556864ff0b848baa182eafbe4e2
lexdumitrache <60842627+lexdumitrache@users.noreply.github.com>
2024-02-21 22:39:56 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const onUserChangedText = (event) => {
+  console.log(event.target.value);
+  setUserInput(event.target.value);
+};
   return (
     <div className="root">
       <div className="container">
@@ -16,7 +22,12 @@ const Home = () => {
         </div>
         {/* Add this code here*/}
         <div className="prompt-container">
-          <textarea placeholder="start typing here" className="prompt-box" />
+          <textarea
+  className="prompt-box"
+  placeholder="start typing here"
+  value={userInput}
+  onChange={onUserChangedText}
+/>;
         </div>
       </div>
       <div className="badge-container grow">
