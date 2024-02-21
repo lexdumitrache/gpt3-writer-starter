60e031a48db456f223ea2c99d621399df6073f4b
lexdumitrache <60842627+lexdumitrache@users.noreply.github.com>
2024-02-21 22:08:07 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,6 @@ import buildspaceLogo from '../assets/buildspace-logo.png';
 const Home = () => {
   return (
     <div className="root">
-      <Head>
-        <title>GPT-3 Writer | buildspace</title>
-      </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
@@ -17,6 +14,10 @@ const Home = () => {
             <h2>insert your subtitle here</h2>
           </div>
         </div>
+        {/* Add this code here*/}
+        <div className="prompt-container">
+          <textarea placeholder="start typing here" className="prompt-box" />
+        </div>
       </div>
       <div className="badge-container grow">
         <a
@@ -24,10 +25,6 @@ const Home = () => {
           target="_blank"
           rel="noreferrer"
         >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
-          </div>
         </a>
       </div>
     </div>
